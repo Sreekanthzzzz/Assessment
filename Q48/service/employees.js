f4c226977061c75b9e80bd5a22b0754e1b0e0601
sreekanthzzzz <bollepalli.sreekanth@incedoinc.com>
2022-12-09 12:06:39 +0530

    initial commit

---
+++ Q48/service/employees.js
@@ -0,0 +1,20 @@
+var employees=[        {
+    "EmployeeID": "0",
+    "EmployeeName": "sreekanth",
+    "Role": "Admin",
+    
+},
+{
+    "EmployeeID": "1",
+    "EmployeeName": "RAVI",
+    "Role": "User",
+    
+},
+{
+    "EmployeeID": "2",
+    "EmployeeName": "suresh",
+    "Role": "Developer",
+    
+}
+];
+module.exports=employees;
